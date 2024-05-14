7272f9dd1302a4726fa5b6f2fdf6c21264e3a4fb
Victor Souza Lima <you@example.com>
2024-05-13 21:18:49 -0300

    *

--- Codigo/backend/java/src/main/java/dao/UsuarioDao.java
+++ Codigo/backend/java/src/main/java/dao/UsuarioDao.java
@@ -12,12 +12,12 @@ import javafx.scene.chart.PieChart.Data;
 
 public interface UsuarioDao {
     @SqlUpdate("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, name VARCHAR, cpf VARCHAR , email VARCHAR, salary FLOAT, cellNumber VARCHAR, password VARCHAR, expenses VARCHAR, regDate DATE)")
-    void createTable();
+    public void createTable();
     
     @SqlUpdate("INSERT INTO user (id, name, cpf, email, salary, cellNumber, password, expenses, regDate) VALUES (:id, :name, :cpf, :email, :salary, :cellNumber, :password, :expenses, :regDate)")
-    void insert(@Bind("id") int id, @Bind("name") String name, @Bind("cpf") String cpf, @Bind("email") String email,  @Bind("salary") Double salary,  @Bind("cellNumber") String cellNumber,  @Bind("password") String password,  @Bind("expenses") Double expenses,  @Bind("regDate") Date regDate);
+    public void insert(@Bind("id") int id, @Bind("name") String name, @Bind("cpf") String cpf, @Bind("email") String email,  @Bind("salary") Double salary,  @Bind("cellNumber") String cellNumber,  @Bind("password") String password,  @Bind("expenses") Double expenses,  @Bind("regDate") Date regDate);
 
     @SqlQuery("SELECT * FROM user WHERE id = 1")
-    List<Usuario> listUsuarios();
+    public List<Usuario> listUsuarios();
     
 }
