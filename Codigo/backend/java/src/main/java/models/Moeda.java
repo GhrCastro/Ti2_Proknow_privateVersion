c5e6648c33539bb5322959c8e8393a964809dad7
João Paulo Dias Estevão <joao.diasest@gmail.com>
2024-05-16 23:05:00 -0300

    setting random ids in range 1-9999 to class Moeda

--- Codigo/backend/java/src/main/java/models/Moeda.java
+++ Codigo/backend/java/src/main/java/models/Moeda.java
@@ -1,52 +1,65 @@
 package models;
 
 import java.math.BigDecimal;
+import java.util.Random;
 
 public class Moeda {
 	private int id;
 	private String name;
 	private BigDecimal amount;
 	private String symbol;
+
 	
-	public Moeda() { }
-	public Moeda(String name, BigDecimal amount, String symbol) { 
-		this.id = (int) Math.random();
+	public Moeda() {
+	}
+
+	public Moeda(String name, BigDecimal amount, String symbol) {
+		this.id = generateRandomId();
 		this.name = name;
 		this.amount = amount;
 		this.symbol = symbol;
 	}
-	
+
+	private int generateRandomId() {
+		// Supondo que queremos IDs entre 1000 e 9999
+		
+		Random random = new Random();
+		
+		int min = 1;
+		int max = 9999;
+		return random.nextInt((max - min) + 1) + min;
+	}
+
 	public int getId() {
-        return id;
-    }
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public BigDecimal getAmount() {
-        return amount;
-    }
+	public BigDecimal getAmount() {
+		return amount;
+	}
 
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
 
-    public String getSymbol() {
-        return symbol;
-    }
+	public String getSymbol() {
+		return symbol;
+	}
 
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
-
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
 
 }
